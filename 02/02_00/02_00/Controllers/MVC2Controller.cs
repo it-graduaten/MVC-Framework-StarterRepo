691b2f1704141b4b197102281578fcbe786ded48
QuirkyDev <joeri.verlooy@gmail.com>
2023-09-06 23:21:49 +0200

    H02_00 first draft

---
+++ 02/02_00/02_00/Controllers/MVC2Controller.cs
@@ -0,0 +1,23 @@
+﻿using _02_00.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _02_00.Controllers
+{
+    public class MVC2Controller : Controller
+    {
+        public List<Klant> klanten = new List<Klant>()
+        {
+            new Klant() { KlantID = 1, Voornaam = "Anneleen", Familienaam = "De Neve", AangemaaktDatum = new DateTime(2019, 1, 20) },
+            new Klant() { KlantID = 2, Voornaam = "Nele", Familienaam = "Bruynseels", AangemaaktDatum = new DateTime(2020, 2, 4) },
+            new Klant() { KlantID = 3, Voornaam = "Joris", Familienaam = "Naert", AangemaaktDatum = new DateTime(2020, 1, 5) },
+            new Klant() { KlantID = 4, Voornaam = "Jan", Familienaam = "Vermeire", AangemaaktDatum = new DateTime(2021, 7, 13) },
+            new Klant() { KlantID = 5, Voornaam = "Sasha", Familienaam = "Van Gompel", AangemaaktDatum = new DateTime(2022, 9, 25) }
+        };
+
+        public IActionResult Index()
+        {
+            List<Klant> alleKlanten = klanten;
+            return View(alleKlanten);
+        }
+    }
+}
---
+++ 02/02_00/02_00/Models/Klant.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _02_00.Models
+{
+    public class Klant
+    {
+        public int KlantID { get; set; }
+
+        public string Voornaam { get; set; }
+
+        public string Familienaam { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime AangemaaktDatum { get; set; }
+    }
+}
